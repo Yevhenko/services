2dd5826510e91f42f6cd3970f07032e3a704eee2
Yevhenko <yevhenkozlyi@gmail.com>
2021-03-05 14:24:58 +0200

    setting up db

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,21 @@
 import { env } from './config/env';
+import 'reflect-metadata';
+
 import express from 'express';
+
+import { createConnection } from 'typeorm';
+// import { User } from './db/entity/User';
 
 const app = express();
 
 const port = env.APP_PORT;
 
-app.listen(port, () => {
-    return console.log(`Server is listening on ${port}`);
-});
+createConnection()
+  .then(async (connection) => {
+    console.log(connection);
+
+    app.listen(port, () => {
+      return console.log(`Server is listening on ${port}`);
+    });
+  })
+  .catch((error) => console.error(error));
