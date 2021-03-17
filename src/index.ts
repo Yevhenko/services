ec2b638e77ee6e2351e70d1df11ac33aba625ad0
Yevhenko <yevhenkozlyi@gmail.com>
2021-03-17 12:41:18 +0200

    logger added

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,10 @@ import { env } from './config/env';
 import 'reflect-metadata';
 
 import express from 'express';
-// import bodyParser from 'body-parser';
+import epl from 'express-pino-logger';
+import bodyParser from 'body-parser';
+
+import { logger } from './logger';
 import router from './server/router';
 import { createConnection } from 'typeorm';
 
@@ -10,16 +13,19 @@ const app = express();
 
 const port = env.APP_PORT;
 
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
+app.use(epl({ logger: logger }));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use(router);
 
 createConnection()
   .then(async (connection) => {
-    console.log('Connection to db is successful');
+    logger.info('Connection to db is successful');
 
     app.listen(port, () => {
-      return console.log(`Server is listening on ${port}`);
+      return logger.info(`Server is listening on ${port}`);
     });
   })
   .catch((error) => console.error(error));
